17687d99dc2067b842a26c6c1c9061b7c4249fef
Linds Gray <lindsaygray11@gmail.com>
2017-11-02 22:45:16 -0700

    feat(photography overview tab): added imgs to tab buttons and finished photography tab

---
+++ src/app/components/hp-photos/hp-photos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hp-photos',
+  templateUrl: './hp-photos.component.html',
+  styleUrls: ['./hp-photos.component.css']
+})
+export class HpPhotosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/overview/overview.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TabsetComponent } from 'ngx-bootstrap';
+
+
+@Component({
+  selector: 'overview-comp',
+  templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.css']
+})
+export class OverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
